fdea2b82ae22d5a803e3aa65e9270ad7974d6347
rvalikov <r.valikov@mail.ru>
2022-12-05 12:55:09 +0300

    Добавил BABEL

---
+++ dist/index.51664ae195cf40420ae9.js
@@ -0,0 +1,1 @@
+(()=>{var e;document.body.prepend(("Проект собран на WEBPACK",(e=document.createElement("h1")).textContent="Проект собран на WEBPACK",e))})();
--- webpack.config.js
+++ webpack.config.js
@@ -3,11 +3,21 @@ const path = require("path");
 const FileManagerPlugin = require('filemanager-webpack-plugin');
 
 module.exports={
+    
     output: {
         path: path.join(__dirname, 'dist'),
         filename: 'index.[contenthash].js',
       },
     entry: path.join(__dirname, 'src', 'index.js'),
+    module: {
+             rules: [
+               {
+                 test: /\.js$/,
+                 use: 'babel-loader',
+                 exclude: /node_modules/,
+               },
+             ],
+           },
 plugins:[
     new HTMLWebpackPlugin({
         template:path.join(__dirname, 'src', 'template.html'),
