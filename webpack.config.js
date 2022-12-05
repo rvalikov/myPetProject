13bd46dc74e4ed42faf23f98f3654d6c80e1144b
rvalikov <r.valikov@mail.ru>
2022-12-05 12:45:06 +0300

    Стадия удаления dist

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,28 @@
 const HTMLWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
+const FileManagerPlugin = require('filemanager-webpack-plugin');
 
 module.exports={
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'index.[contenthash].js',
+      },
+    entry: path.join(__dirname, 'src', 'index.js'),
 plugins:[
     new HTMLWebpackPlugin({
         template:path.join(__dirname, 'src', 'template.html'),
         filename: 'index.html',
     }),
+    new FileManagerPlugin({
+               events: {
+                 onStart: {
+                   delete: ['dist'],
+                 },
+               },
+             }),
 ],
+devServer:{
+    watchFiles: path.join(__dirname, 'src'),
+    port:9000,
+}
 }
