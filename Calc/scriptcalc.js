780d0e021bb740b1715a560711836b583155dd4c
rvalikov <r.valikov@mail.ru>
2022-12-13 12:15:08 +0300

    Добавил ввод с клавиатуры

--- Calc/scriptcalc.js
+++ Calc/scriptcalc.js
@@ -3,12 +3,15 @@ let button = document.querySelectorAll(".button")
 let operation = document.querySelectorAll(".operation")
 let equal = document.querySelector(".equal")
 let clear = document.querySelector(".clear")
+input.focus()
+input.addEventListener("keyup", NumLock)
 clear.addEventListener("click", clearfunc)
 equal.addEventListener("click", eq)
 let temp=0
-let op
+let res=0
+let textOrNum =0// переменная хранит информацию откуда вводится арифметическое действие
+let op =" " // переменная знака арифмитического действия
 for(let elem of button){
-    console.log(elem.textContent)
     elem.addEventListener("click", write)
 }
 for(let elem of operation){
@@ -18,29 +21,39 @@ function write(){
     input.value+=this.textContent
 }
 function oper(){
-    op=this.textContent
-    console.log(op)
-     temp=parseFloat (input.value)
+   if (textOrNum==1){op=event.key
+console.log("NUM " + op)}
+   else
+    {op=this.textContent
+    
+    console.log("TEXT" + op)}
+ temp=parseFloat (input.value)
     input.value=" "
+    
 
 }
 function eq(){
 if (op=="+")
-     {res=Number (temp)+Number ( (input.value))
-    input.value=res}
+     {res=Number (temp)+Number ( (input.value))}
     else if (op=="-")
-    {res=Number (temp)-Number ( (input.value))
-        input.value=res}
+    {res=Number (temp)-Number ( (input.value))}
         else if (op=="*")
-        {res=Number (temp)*Number ( (input.value))
-            input.value=res}
+        {res=Number (temp)*Number ( (input.value))}
             else if (op=="/")
-            {res=Number (temp)/Number ( (input.value))
-                input.value=res}
-               
+            {res=Number (temp)/Number ( (input.value))}
+            input.value=" "
+            input.value=res             
 }
 function clearfunc(){
     input.value=" "
+    input.focus()
+}
+  function NumLock()
+  {
+  textOrNum=1
+  console.log(event.key);
+  if (event.key==="+"||event.key==="-"||event.key==="*"||event.key==="/"){oper()}
+  else if (event.key=="Enter"){eq()}
 }
 
 
